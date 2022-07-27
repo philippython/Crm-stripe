620f32ea839bd39dbb21219ff5385a93e4de4819
odulaja philip <odulajaphilip@gmail.com>
2022-07-27 11:40:01 +0100

    created detail and update view

--- crmapp/account/urls.py
+++ crmapp/account/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url, include
-from .views import AccountList
+from .views import AccountList, AccountDetail, AccountCreate, AccountEdit
 
 urlpatterns = [
-        url(r'^list/$', AccountList.as_view(), name='account_list')
+        url(r'^list/$', AccountList.as_view(), name='account_list'),
+        url(r'^create/$', AccountCreate.as_view(), name='account_create'),
+        url(r'^detail/(?P<pk>[\w-]+)/', AccountDetail.as_view(), name='account_detail'),
+        url(r'^edit/(?P<pk>[\w-]+)/', AccountEdit.as_view(), name='account_edit')
 ]
--- crmapp/account/views.py
+++ crmapp/account/views.py
@@ -2,19 +2,39 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 from .models import Account
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, DetailView, UpdateView
 # Create your views here.
 
 class AccountList(ListView):
     template_name = 'account/account_list.html'
     model = Account
-    paginate_by = 10
+    paginate_by = 2
     context_object_name = 'accounts'
 
     def get_queryset(self):
-        accounts_list = Account.objects.filter(owner=self.request.user)
+        try:
+            a = self.request.GET.get('account')
+        except KeyError:
+            a = None
+        if a:
+            accounts_list = Account.objects.filter(name__icontains= a,
+                            owner=self.request.user)
+        else:
+            accounts_list = Account.objects.filter(owner=self.request.user)
         return accounts_list
 
     @method_decorator(login_required)
     def dispatch(self, *args, **kwargs):
         return super(AccountList, self).dispatch(*args, **kwargs)
+
+
+class AccountDetail(DetailView):
+    model = Account
+
+
+class AccountCreate(CreateView):
+    model = Account
+
+class AccountEdit(UpdateView):
+    model = Account
+    fields = ('name', 'desc')
